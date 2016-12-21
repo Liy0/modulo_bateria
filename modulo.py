11631ee70d22fd8a2980e66670d3f611245d814b
Pablo Martin Perez <pmperez@digicard.net>
2016-12-21 17:55:14 -0300

    updates

--- modulo.py
+++ modulo.py
@@ -2,8 +2,8 @@
 
 """beetbox.py: Trigger script for the BeetBox."""
 
-__author__ = "Scott Garner"
-__email__ = "scott@j38.net"
+__author__ = "Pablo Perez"
+__email__ = "pablomartin.it@gmail.com"
 
 import pygame
 import readchar
@@ -13,24 +13,82 @@ import readchar
 pygame.mixer.pre_init(44100, -16, 12, 512)
 pygame.init()
 
-kick = pygame.mixer.Sound('samples/kick.wav')
-kick.set_volume(.65);
-snare = pygame.mixer.Sound('samples/snare.wav')
-snare.set_volume(.65);
-openhh = pygame.mixer.Sound('samples/open.wav')
-openhh.set_volume(.65);
-closedhh = pygame.mixer.Sound('samples/closed.wav')
-closedhh.set_volume(.65);
-clap = pygame.mixer.Sound('samples/clap.wav')
-clap.set_volume(.65);
-cymbal = pygame.mixer.Sound('samples/cymbal.wav')
-cymbal.set_volume(.65);
+class Modulo:
 
-# Track touches
+    def __init__(self):
+       self.SOUND_MAPPING = {
+            0: 'kick.wav',
+            1: 'snare.wav',
+            2: 'open.wav',
+            3: 'closed.wav',
+            4: 'clap.wav',
+            5: 'cymbal.wav'
+       }
+       self.cuerpos = [0,0,0,0,0,0]
 
-touches = [0,0,0,0,0,0];
+    def kit(self,kit):
+        for key, soundfile in self.SOUND_MAPPING.iteritems():
+            self.cuerpos[key] = pygame.mixer.Sound(kit+"/"+soundfile)
+            self.cuerpos[key].set_volume(0.1);
+
+    def mas_volumen(self):
+
+        v = self.cuerpos[0].get_volume() + (self.cuerpos[0].get_volume() * 0.30)
+        print v
+        v_int = int(round(v*100))
+        if v_int > 100:
+            v = 100
+        if v_int == 0:
+            v = 1
+        print v_int
+        for c in self.cuerpos:
+            c.set_volume( v );
+
+    def menos_volumen(self):
+        v = self.cuerpos[0].get_volume() - (self.cuerpos[0].get_volume() * 0.30)
+        v_int = int(round(v*100))
+
+        if v_int < 0:
+            v_int = 0
+
+        print v_int
+
+        for c in self.cuerpos:
+            c.set_volume( v_int );
+
+m = Modulo();
+m.kit("samples")
 
 while True:
     c = readchar.readchar()
-    print c
-    kick.play()
+    #print ord(c)
+
+    if (ord(c) == 44):
+        m.menos_volumen()
+        continue
+
+    if (ord(c) == 46):
+        m.mas_volumen()
+        continue
+
+    if (ord(c) == 97):  # A
+        m.cuerpos[0].play()
+
+    if (ord(c) == 115):  # S
+        m.cuerpos[1].play()
+
+    if (ord(c) == 100):  # D
+        m.cuerpos[2].play()
+
+    if (ord(c) == 106):  # J
+        m.cuerpos[3].play()
+
+    if (ord(c) == 107):  # K
+        m.cuerpos[4].play()
+
+    if (ord(c) == 108):  # L
+        m.cuerpos[5].play()
+
+    if (ord(c) == 3):
+        print "Chaucha..."
+        break
